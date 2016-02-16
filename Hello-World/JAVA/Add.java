37e9a59944bc53fea339f2d1a81225ac610c9425
Aman Kumar Singh <amanami00@gmail.com>
2016-02-16 12:38:15 +0530

    Create Add.java

---
+++ Hello-World/JAVA/Add.java
@@ -0,0 +1,11 @@
+class AddNumber   
+{
+   public static void main(String args[]) 
+   {
+      int x, y, z;     
+      x = 10;          
+      y = 20;          
+      z = x + y;       
+      System.out.println("Sum of x and y = "+z); 
+   }
+}

c8f98b77256a5b80ddd463d68a81eaa04f5a6f45
Aman Kumar Singh <amanami00@gmail.com>
2016-02-16 12:02:08 +0530

    Create hello.cpp

---
+++ Hello-World/CPP/hello.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+ int a,b,c;
+cout << "Enter two numbers to add\n";
+cin >> a >> b;
+c=a+b;
+cout << "sum of two entered numbers= " << c ;
+return 0;
+
+}
